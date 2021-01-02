d0d51069d97a3909d522c03e9f00b48b9002814c
Francisco Cunha <francisco.97@live.com.pt>
2021-01-02 22:32:40 +0000

    WebServiceProject

---
+++ src/main/java/io/javabrains/esofproj/EsofProjApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.esofproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsofProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsofProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/javabrains/esofproj/EsofProjApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.esofproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsofProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
